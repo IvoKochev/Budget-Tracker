5cfd22df22a0eb7046e5c0eca7a851fe89b4548e
Slavi Vatahov <slavchovatahov94@gmail.com>
2017-09-14 13:23:48 +0300

    fix registration bugs

--- BudgetTracker/src/java/com/database/RegistrationServlet.java
+++ BudgetTracker/src/java/com/database/RegistrationServlet.java
@@ -9,7 +9,9 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
@@ -22,50 +24,73 @@ import javax.servlet.http.HttpServletResponse;
  * @author slavi
  */
 public class RegistrationServlet extends HttpServlet {
-
+    
     private Connection connect = null;
-
+    
     @Override
-    protected void doPost(HttpServletRequest request,
-            HttpServletResponse response) throws ServletException, IOException {
-        // response.sendRedirect("/BudgetTracker/secure.budgettracker.com/createuser.jsp");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
         String emailaddress = request.getParameter("emailaddress");
         String password = request.getParameter("password");
-        if (this.openConnection()) {
-            this.write(emailaddress, password,response);
+        String password2 = request.getParameter("password2");
+        
+        if (!this.checkPassword(password, password2)) {
+            response.sendRedirect("/BudgetTracker/secure.budgettracker.com/createuser.jsp");
+            return;
         }
+        if (this.openConnection()) {
+            if (!checkEmail(emailaddress)) {
+                this.write(emailaddress, password);
+                return;
+            }
+            response.sendRedirect("/BudgetTracker/secure.budgettracker.com/createuser.jsp");
+        }
+        response.getWriter().print("Connection problem.Please try later");
     }
-
+    
     private boolean openConnection() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             String url = "jdbc:mysql://localhost:3306/budgettracker";
             connect = DriverManager.getConnection(url, "root", "1234");
-
+            
         } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(RegistrationServlet.class.getName()).log(Level.SEVERE, null, ex);
             return false;
         }
         return true;
     }
-
-    private void write(String email, String password, HttpServletResponse response) {
+    
+    private void write(String email, String password) {
         try {
             String query = " insert into accounts (email,password) values (?, ?)";
-            PreparedStatement preparedStmt = connect.prepareStatement(query,PreparedStatement.RETURN_GENERATED_KEYS);
-
+            PreparedStatement preparedStmt = connect.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
+            
             preparedStmt.setString(1, email);
             preparedStmt.setString(2, password);
-
+            
             preparedStmt.execute();
             connect.close();
         } catch (SQLException ex) {
-            try {
-                response.getWriter().print(ex);
-                Logger.getLogger(RegistrationServlet.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (IOException ex1) {
-                Logger.getLogger(RegistrationServlet.class.getName()).log(Level.SEVERE, null, ex1);
-            }
+            Logger.getLogger(RegistrationServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    private boolean checkPassword(String password, String password2) {
+        return password.equals(password2);
+    }
+    
+    private boolean checkEmail(String email) {
+        try {
+            String query = "SELECT * FROM accounts WHERE email ='" + email + "'";
+            Statement stmt = connect.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            if (rs.next()) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(RegistrationServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
     }
 }
