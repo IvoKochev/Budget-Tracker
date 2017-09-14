246333bad6b3057d95899408f6bdf4c12e83537b
Slavi Vatahov <slavchovatahov94@gmail.com>
2017-09-14 11:20:14 +0300

    create connection to database

--- BudgetTracker/src/java/com/database/RegistrationServlet.java
+++ BudgetTracker/src/java/com/database/RegistrationServlet.java
@@ -6,6 +6,12 @@
 package com.database;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,13 +23,49 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class RegistrationServlet extends HttpServlet {
 
+    private Connection connect = null;
+
     @Override
     protected void doPost(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
-
+        // response.sendRedirect("/BudgetTracker/secure.budgettracker.com/createuser.jsp");
         String emailaddress = request.getParameter("emailaddress");
         String password = request.getParameter("password");
-        response.getWriter().print(emailaddress + " " + password);
-        response.sendRedirect("/BudgetTracker/secure.budgettracker.com/createuser.jsp");
+        if (this.openConnection()) {
+            this.write(emailaddress, password,response);
+        }
+    }
+
+    private boolean openConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/budgettracker";
+            connect = DriverManager.getConnection(url, "root", "1234");
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(RegistrationServlet.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+        return true;
+    }
+
+    private void write(String email, String password, HttpServletResponse response) {
+        try {
+            String query = " insert into accounts (email,password) values (?, ?)";
+            PreparedStatement preparedStmt = connect.prepareStatement(query,PreparedStatement.RETURN_GENERATED_KEYS);
+
+            preparedStmt.setString(1, email);
+            preparedStmt.setString(2, password);
+
+            preparedStmt.execute();
+            connect.close();
+        } catch (SQLException ex) {
+            try {
+                response.getWriter().print(ex);
+                Logger.getLogger(RegistrationServlet.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex1) {
+                Logger.getLogger(RegistrationServlet.class.getName()).log(Level.SEVERE, null, ex1);
+            }
+        }
     }
 }
