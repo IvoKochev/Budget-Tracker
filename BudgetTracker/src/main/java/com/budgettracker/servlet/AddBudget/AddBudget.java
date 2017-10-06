142b7eabf402e865338aae68db3cbd8d10208796
S_Vatahov <slavchovatahov94@gmail.com>
2017-10-06 16:42:37 +0300

    some changes

--- BudgetTracker/src/main/java/com/budgettracker/servlet/AddBudget/AddBudget.java
+++ BudgetTracker/src/main/java/com/budgettracker/servlet/AddBudget/AddBudget.java
@@ -13,8 +13,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
